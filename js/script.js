9cac396f93e79a79c9d927ed28893c11c6ac1123
SaneDoubleTea <doubletea2015@gmail.com>
2023-10-06 14:07:49 +0300

    intro, modal, cookies done

--- js/script.js
+++ js/script.js
@@ -32,84 +32,38 @@
 //         dragSize: 100,
 //     },
 // });
+//!====== Burger Menu=========
+document.addEventListener('DOMContentLoaded', function burgerMenu() {
+    const navBtn = document.querySelector('.nav-btn'),
+        mobileNav = document.querySelector('.mobile-nav'),
+        body = document.body;
 
-//!======Modal=========
-MicroModal.init({
-    // onShow: modal => console.info(`${modal.id} is shown`), // [1]
-    // onClose: modal => console.info(`${modal.id} is hidden`), // [2]
-    openTrigger: 'data-micromodal-trigger', // [3]
-    closeTrigger: 'data-custom-close', // [4]
-    disableScroll: true, // [5]
-    disableFocus: false, // [6]
-    awaitOpenAnimation: false, // [7]
-    awaitCloseAnimation: false, // [8]
-    debugMode: true, // [9]
-});
-
-//!ACCORDION
-// import Accordion from "accordion-js";
-//var accordion = new Accordion(".accordion-container");
-// var accordion = new Accordion(".accordion-container", {
-//     // animation duration in ms {number}
-//     duration: 600,
-//     // add ARIA elements to the HTML structure {boolean}
-//     ariaEnabled: true,
-//     // allow collapse expanded panel {boolean}
-//     collapse: true,
-//     // show multiple elements at the same time {boolean}
-//     showMultiple: false,
-//     // show accordion elements during initialization {array}
-//     openOnInit: [],
-//     // element class {string}
-//     elementClass: "ac",
-//     // trigger class {string}
-//     triggerClass: "ac-trigger",
-//     // panel class {string}
-//     panelClass: "ac-panel",
-//     // active element class {string}
-//     activeClass: "is-active",
-// });
-
-//!BURGER MENU
-const navBtn = document.querySelector('.nav-btn');
-const mobileNav = document.querySelector('.mobile-nav');
-const body = document.body;
-
-//Клик по кнопке
-navBtn.addEventListener('click', function (event) {
-    event.stopPropagation();
-    toggleMobilenav();
-});
-
-//Клик по окну за пределами навигации
-window.addEventListener('click', function () {
-    if (body.classList.contains('no-scroll')) {
+    //Клик по кнопке
+    navBtn.addEventListener('click', function (event) {
+        event.stopPropagation();
         toggleMobilenav();
+    });
+
+    //Клик по окну за пределами навигации
+    window.addEventListener('click', function () {
+        if (body.classList.contains('no-scroll')) {
+            toggleMobilenav();
+        }
+    });
+
+    //останавливаем клик внутри открытой мобильной навигации
+    mobileNav.addEventListener('click', function (event) {
+        event.stopPropagation();
+    });
+
+    function toggleMobilenav() {
+        mobileNav.classList.toggle('mobile-nav-active');
+        navBtn.classList.toggle('nav-btn-close');
+        body.classList.toggle('no-scroll');
     }
 });
-//останавливаем клик внутри открытой мобильной навигации
-mobileNav.addEventListener('click', function (event) {
-    event.stopPropagation();
-});
 
-function toggleMobilenav() {
-    mobileNav.classList.toggle('mobile-nav-active');
-    navBtn.classList.toggle('nav-btn-close');
-    body.classList.toggle('no-scroll');
-}
-
-//!статичный хедер при скролле
-(function () {
-    const header = document.querySelector('.header');
-    window.onscroll = () => {
-        if (window.pageYOffset > 400) {
-            header.classList.add('header--active');
-        } else {
-            header.classList.remove('header--active');
-        }
-    };
-})();
-
+//!======Cookies=========
 document.addEventListener('DOMContentLoaded', function cookies() {
     const cookiesPopup = document.querySelector('.cookies');
     const okButton = document.querySelector('#btn-ok');
@@ -136,6 +90,70 @@ document.addEventListener('DOMContentLoaded', function cookies() {
         }
     });
 });
+
+//!======Modal=========
+document.addEventListener('DOMContentLoaded', function modalWindow() {
+    MicroModal.init({
+        openTrigger: 'data-micromodal-trigger',
+        closeTrigger: 'data-custom-close',
+        disableScroll: true,
+        disableFocus: false,
+        awaitOpenAnimation: false,
+        awaitCloseAnimation: false,
+        debugMode: true,
+    });
+});
+
+//!======Radio Buttons=========
+document.addEventListener('DOMContentLoaded', function RadioButtons() {
+    const customRadios = document.querySelectorAll('.custom-radio');
+
+    customRadios.forEach((customRadio) => {
+        const radioInput = customRadio.querySelector('input[type="radio"]');
+
+        radioInput.addEventListener('change', () => {
+            customRadios.forEach((otherRadio) => {
+                otherRadio.classList.remove('custom-radio--active');
+            });
+
+            if (radioInput.checked) {
+                customRadio.classList.add('custom-radio--active');
+            }
+        });
+    });
+});
+
+// !======Radio Linsk===========
+document.addEventListener('DOMContentLoaded', function () {
+    const personalRadio = document.getElementById('personalRadio');
+    const organizationRadio = document.getElementById('organizationRadio');
+    const nextButton = document.getElementById('nextButton');
+
+    const personalLink = 'https://google.com';
+    const organizationLink = 'https://google.com/images';
+
+    let selectedOption = null;
+
+    personalRadio.addEventListener('change', function () {
+        selectedOption = personalLink;
+    });
+
+    organizationRadio.addEventListener('change', function () {
+        selectedOption = organizationLink;
+    });
+
+    nextButton.addEventListener('click', function (e) {
+        e.preventDefault(); // Предотвращаем стандартное действие ссылки.
+
+        if (selectedOption !== null) {
+            // Открываем выбранную ссылку в новом окне.
+            window.open(selectedOption, '_blank');
+        } else {
+            alert('Please choose option.');
+        }
+    });
+});
+
 //======Swiper=========
 const fraction = document.getElementById('fraction');
 const slides = document.querySelectorAll('.swiper-slide');
@@ -161,24 +179,26 @@ new Swiper('.benefits__slider', {
     },
 });
 
-// Получаем все элементы <label> с классом "custom-radio"
-const customRadios = document.querySelectorAll('.custom-radio');
-
-// Добавляем обработчик события для каждого <label>
-customRadios.forEach((customRadio) => {
-    // Получаем связанный с ним <input>
-    const radioInput = customRadio.querySelector('input[type="radio"]');
-
-    // Добавляем обработчик события change для <input>
-    radioInput.addEventListener('change', () => {
-        // Снимаем класс "custom-radio--active" со всех элементов <label> сразу
-        customRadios.forEach((otherRadio) => {
-            otherRadio.classList.remove('custom-radio--active');
-        });
-
-        // Если <input> становится checked, добавляем класс "custom-radio--active" к текущему <label>
-        if (radioInput.checked) {
-            customRadio.classList.add('custom-radio--active');
-        }
-    });
-});
+//!ACCORDION
+// import Accordion from "accordion-js";
+//var accordion = new Accordion(".accordion-container");
+// var accordion = new Accordion(".accordion-container", {
+//     // animation duration in ms {number}
+//     duration: 600,
+//     // add ARIA elements to the HTML structure {boolean}
+//     ariaEnabled: true,
+//     // allow collapse expanded panel {boolean}
+//     collapse: true,
+//     // show multiple elements at the same time {boolean}
+//     showMultiple: false,
+//     // show accordion elements during initialization {array}
+//     openOnInit: [],
+//     // element class {string}
+//     elementClass: "ac",
+//     // trigger class {string}
+//     triggerClass: "ac-trigger",
+//     // panel class {string}
+//     panelClass: "ac-panel",
+//     // active element class {string}
+//     activeClass: "is-active",
+// });
