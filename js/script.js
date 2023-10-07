c47022729b5959f33901257ee966c0e8a4bd7624
SaneDoubleTea <doubletea2015@gmail.com>
2023-10-07 15:38:57 +0300

    accordion pc

--- js/script.js
+++ js/script.js
@@ -1,68 +1,3 @@
-//!======Swiper=========
-// new Swiper(".swiper", {
-//     modules: [Navigation, Pagination, EffectFade, Scrollbar],
-//     loop: false,
-//     speed: 800,
-//     effect: "fade",
-//     spaceBetween: 30,
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//         /*  type: "fraction", */
-//         //custom bullets
-//         /* renderBullet: function (index, className) {
-//             return '<span class="' + className + '">' + (index + 1) + "</span>";
-//         }, */
-
-//         /* type: "custom",
-//         renderCustom: function (swiper, current, total) {
-//             let indT = total >= 10 ? total : `0${total}`;
-//             let indC = current >= 10 ? current : `0${current}`;
-//             return `<b>${indC}</b> <span></span>${indT}`;
-//         }, */
-//     },
-//     scrollbar: {
-//         el: ".swiper-scrollbar",
-//         hide: false,
-//         draggable: true,
-//         dragSize: 100,
-//     },
-// });
-//!====== Burger Menu=========
-document.addEventListener('DOMContentLoaded', function burgerMenu() {
-    const navBtn = document.querySelector('.nav-btn'),
-        mobileNav = document.querySelector('.mobile-nav'),
-        body = document.body;
-
-    //Клик по кнопке
-    navBtn.addEventListener('click', function (event) {
-        event.stopPropagation();
-        toggleMobilenav();
-    });
-
-    //Клик по окну за пределами навигации
-    window.addEventListener('click', function () {
-        if (body.classList.contains('no-scroll')) {
-            toggleMobilenav();
-        }
-    });
-
-    //останавливаем клик внутри открытой мобильной навигации
-    mobileNav.addEventListener('click', function (event) {
-        event.stopPropagation();
-    });
-
-    function toggleMobilenav() {
-        mobileNav.classList.toggle('mobile-nav-active');
-        navBtn.classList.toggle('nav-btn-close');
-        body.classList.toggle('no-scroll');
-    }
-});
-
 //!======Cookies=========
 document.addEventListener('DOMContentLoaded', function cookies() {
     const cookiesPopup = document.querySelector('.cookies');
@@ -186,25 +121,237 @@ new Swiper('.benefits__slider', {
     },
 });
 
-//!ACCORDION
-// import Accordion from "accordion-js";
-let accordion = new Accordion('.accordion-container', {
-    // animation duration in ms {number}
-    duration: 600,
-    // add ARIA elements to the HTML structure {boolean}
-    ariaEnabled: true,
-    // allow collapse expanded panel {boolean}
-    collapse: true,
-    // show multiple elements at the same time {boolean}
-    showMultiple: false,
-    // show accordion elements during initialization {array}
-    openOnInit: [],
-    // element class {string}
-    elementClass: 'ac',
-    // trigger class {string}
-    triggerClass: 'ac-trigger',
-    // panel class {string}
-    panelClass: 'ac-panel',
-    // active element class {string}
-    activeClass: 'is-active',
+document.addEventListener('DOMContentLoaded', function () {
+    //!ACCORDION
+    let accordion = new Accordion('.accordion-container', {
+        duration: 600,
+        ariaEnabled: true,
+        collapse: true,
+        showMultiple: false,
+        openOnInit: [],
+        elementClass: 'ac',
+        triggerClass: 'ac-trigger',
+        panelClass: 'ac-panel',
+        activeClass: 'is-active',
+    });
+
+    accordion.open(0);
+});
+
+function DynamicAdapt() {
+    // Динамический адаптив
+    function DynamicAdapt(type) {
+        this.type = type;
+    }
+    DynamicAdapt.prototype.init = function () {
+        const _this = this;
+        // массив объектов
+        this.оbjects = [];
+        this.daClassname = '_dynamic_adapt_';
+        // массив DOM-элементов
+        this.nodes = document.querySelectorAll('[data-da]');
+        // наполнение оbjects объктами
+        for (let i = 0; i < this.nodes.length; i++) {
+            const node = this.nodes[i];
+            const data = node.dataset.da.trim();
+            const dataArray = data.split(',');
+            const оbject = {};
+            оbject.element = node;
+            оbject.parent = node.parentNode;
+            оbject.destination = document.querySelector(dataArray[0].trim());
+            оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : '767';
+            оbject.place = dataArray[2] ? dataArray[2].trim() : 'last';
+            оbject.index = this.indexInParent(оbject.parent, оbject.element);
+            this.оbjects.push(оbject);
+        }
+        this.arraySort(this.оbjects);
+        // массив уникальных медиа-запросов
+        this.mediaQueries = Array.prototype.map.call(
+            this.оbjects,
+            function (item) {
+                return '(' + this.type + '-width: ' + item.breakpoint + 'px),' + item.breakpoint;
+            },
+            this
+        );
+        this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, function (item, index, self) {
+            return Array.prototype.indexOf.call(self, item) === index;
+        });
+        // навешивание слушателя на медиа-запрос
+        // и вызов обработчика при первом запуске
+        for (let i = 0; i < this.mediaQueries.length; i++) {
+            const media = this.mediaQueries[i];
+            const mediaSplit = String.prototype.split.call(media, ',');
+            const matchMedia = window.matchMedia(mediaSplit[0]);
+            const mediaBreakpoint = mediaSplit[1];
+            // массив объектов с подходящим брейкпоинтом
+            const оbjectsFilter = Array.prototype.filter.call(this.оbjects, function (item) {
+                return item.breakpoint === mediaBreakpoint;
+            });
+            matchMedia.addListener(function () {
+                _this.mediaHandler(matchMedia, оbjectsFilter);
+            });
+            this.mediaHandler(matchMedia, оbjectsFilter);
+        }
+    };
+    DynamicAdapt.prototype.mediaHandler = function (matchMedia, оbjects) {
+        if (matchMedia.matches) {
+            for (let i = 0; i < оbjects.length; i++) {
+                const оbject = оbjects[i];
+                оbject.index = this.indexInParent(оbject.parent, оbject.element);
+                this.moveTo(оbject.place, оbject.element, оbject.destination);
+            }
+        } else {
+            //for (let i = 0; i < оbjects.length; i++) {
+            for (let i = оbjects.length - 1; i >= 0; i--) {
+                const оbject = оbjects[i];
+                if (оbject.element.classList.contains(this.daClassname)) {
+                    this.moveBack(оbject.parent, оbject.element, оbject.index);
+                }
+            }
+        }
+    };
+    // Функция перемещения
+    DynamicAdapt.prototype.moveTo = function (place, element, destination) {
+        element.classList.add(this.daClassname);
+        if (place === 'last' || place >= destination.children.length) {
+            destination.insertAdjacentElement('beforeend', element);
+            return;
+        }
+        if (place === 'first') {
+            destination.insertAdjacentElement('afterbegin', element);
+            return;
+        }
+        destination.children[place].insertAdjacentElement('beforebegin', element);
+    };
+    // Функция возврата
+    DynamicAdapt.prototype.moveBack = function (parent, element, index) {
+        element.classList.remove(this.daClassname);
+        if (parent.children[index] !== undefined) {
+            parent.children[index].insertAdjacentElement('beforebegin', element);
+        } else {
+            parent.insertAdjacentElement('beforeend', element);
+        }
+    };
+    // Функция получения индекса внутри родителя
+    DynamicAdapt.prototype.indexInParent = function (parent, element) {
+        const array = Array.prototype.slice.call(parent.children);
+        return Array.prototype.indexOf.call(array, element);
+    };
+    // Функция сортировки массива по breakpoint и place
+    // по возрастанию для this.type = min
+    // по убыванию для this.type = max
+    DynamicAdapt.prototype.arraySort = function (arr) {
+        if (this.type === 'min') {
+            Array.prototype.sort.call(arr, function (a, b) {
+                if (a.breakpoint === b.breakpoint) {
+                    if (a.place === b.place) {
+                        return 0;
+                    }
+
+                    if (a.place === 'first' || b.place === 'last') {
+                        return -1;
+                    }
+
+                    if (a.place === 'last' || b.place === 'first') {
+                        return 1;
+                    }
+
+                    return a.place - b.place;
+                }
+
+                return a.breakpoint - b.breakpoint;
+            });
+        } else {
+            Array.prototype.sort.call(arr, function (a, b) {
+                if (a.breakpoint === b.breakpoint) {
+                    if (a.place === b.place) {
+                        return 0;
+                    }
+
+                    if (a.place === 'first' || b.place === 'last') {
+                        return 1;
+                    }
+
+                    if (a.place === 'last' || b.place === 'first') {
+                        return -1;
+                    }
+
+                    return b.place - a.place;
+                }
+
+                return b.breakpoint - a.breakpoint;
+            });
+            return;
+        }
+    };
+    const da = new DynamicAdapt('max');
+    da.init();
+
+    const isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function () {
+            return (
+                isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()
+            );
+        },
+    };
+}
+
+DynamicAdapt();
+
+//!====== Burger Menu=========
+document.addEventListener('DOMContentLoaded', function burgerMenu() {
+    //!BURGER MENU
+    const navBtn = document.querySelector('.nav-btn');
+    const mobileNav = document.querySelector('.mobile-nav');
+    const body = document.body;
+
+    //Клик по кнопке
+    navBtn.addEventListener('click', function (event) {
+        event.stopPropagation();
+        toggleMobilenav();
+    });
+
+    //Клик по окну за пределами навигации
+    window.addEventListener('click', function () {
+        if (body.classList.contains('no-scroll')) {
+            toggleMobilenav();
+        }
+    });
+    //останавливаем клик внутри открытой мобильной навигации
+    mobileNav.addEventListener('click', function (event) {
+        event.stopPropagation();
+    });
+
+    function toggleMobilenav() {
+        mobileNav.classList.toggle('mobile-nav-active');
+        navBtn.classList.toggle('nav-btn-close');
+        body.classList.toggle('no-scroll');
+    }
+
+    //!статичный хедер при скролле
+    (function () {
+        const header = document.querySelector('.header');
+        window.onscroll = () => {
+            if (window.pageYOffset > 400) {
+                header.classList.add('header--active');
+            } else {
+                header.classList.remove('header--active');
+            }
+        };
+    })();
 });
